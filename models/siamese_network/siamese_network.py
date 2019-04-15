ba18a29c88f23e0ed28c3a6945bb54bc2a6b5ed7
martaskrt <skretam@mcmaster.ca>
2019-04-15 15:55:42 -0400

    added linear layer

--- models/siamese_network/siamese_network.py
+++ models/siamese_network/siamese_network.py
@@ -73,6 +73,10 @@ class SiamNet(nn.Module):
         self.fc7.add_module('relu7', nn.ReLU(inplace=True))
         self.fc7.add_module('drop7', nn.Dropout(p=0.5))
 
+        self.fc7.add_module('fc7', nn.Linear(4096, 128))
+        self.fc7.add_module('relu7', nn.ReLU(inplace=True))
+        self.fc7.add_module('drop7', nn.Dropout(p=0.5))
+
         self.classifier = nn.Sequential()
         self.classifier.add_module('fc8', nn.Linear(4096, classes))
 
