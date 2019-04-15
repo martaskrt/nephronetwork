d61242147f4bdaf1304f580a670329503c4c8a88
martaskrt <skretam@mcmaster.ca>
2019-04-15 14:32:54 -0400

    fixed variable name discrepency

--- models/siamese_network/siamese_network.py
+++ models/siamese_network/siamese_network.py
@@ -198,7 +198,7 @@ def train(args, train_dataset, val_dataset, max_epochs):
 
                     accurate_labels_val += torch.sum(torch.argmax(output, dim=1) == target).cpu()
                     all_labels_val += len(target)
-                    
+
                     print(process_results.get_metrics(y_score=output, y_true=target))
 
                 accuracy = 100. * accurate_labels_val / all_labels_val
