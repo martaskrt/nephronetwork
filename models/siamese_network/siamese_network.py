89bc6f827d692d41702c7fbb4ed8bea1ef43c59f
martaskrt <skretam@mcmaster.ca>
2019-04-15 17:30:53 -0400

    changed laye rnames to match with pretrained

--- models/siamese_network/siamese_network.py
+++ models/siamese_network/siamese_network.py
@@ -156,6 +156,7 @@ def train(args, train_dataset, val_dataset, max_epochs):
 
     # 1. filter out unnecessary keys
     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+    pretrained_dict['fc6.fc6_s1.weight'] = pretrained_dict['fc6.fc6_s1.weight'].view(1024, 256, 3, 3)
     # 2. overwrite entries in the existing state dict
     model_dict.update(pretrained_dict)
     # 3. load the new state dict
