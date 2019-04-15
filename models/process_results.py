c598b193a6eab184c77601c5d2098c1ede4fd0b2
martaskrt <skretam@mcmaster.ca>
2019-04-15 14:32:54 -0400

    fixed variable name discrepency

--- models/process_results.py
+++ models/process_results.py
@@ -3,13 +3,13 @@
 
 from sklearn import metrics
 
-def get_metrics(y_pred, y_true):
+def get_metrics(y_score, y_true):
 
-    fpr, tpr, thresholds = metrics.roc_curve(y_score=y_pred, y_true=y_true)
+    fpr, tpr, thresholds = metrics.roc_curve(y_score=y_score, y_true=y_true)
     fnr = 1 - tpr
     tnr = 1 - fpr
     auc = metrics.auc(fpr, tpr)
-    auprc = metrics.average_precision_score(y_score=y_pred, y_true=y_true)
+    auprc = metrics.average_precision_score(y_score=y_score, y_true=y_true)
 
     results = {'fpr': fpr,
                'tpr': tpr,
