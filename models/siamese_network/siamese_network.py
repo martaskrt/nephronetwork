486320a557fa042e24cd09fcb7f6650ffedd91e6
martaskrt <skretam@mcmaster.ca>
2019-04-15 14:40:15 -0400

    taking max of target column

--- models/siamese_network/siamese_network.py
+++ models/siamese_network/siamese_network.py
@@ -198,8 +198,9 @@ def train(args, train_dataset, val_dataset, max_epochs):
 
                     accurate_labels_val += torch.sum(torch.argmax(output, dim=1) == target).cpu()
                     all_labels_val += len(target)
-                    print(torch.argmax(output, dim=1))
-                    print(process_results.get_metrics(y_score=output.cpu().numpy(), y_true=target.cpu().numpy()))
+
+                    class_pred = torch.argmax(output, dim=1)
+                    print(process_results.get_metrics(y_score=class_pred.cpu().numpy(), y_true=target.cpu().numpy()))
 
                 accuracy = 100. * accurate_labels_val / all_labels_val
                 print('Test accuracy: {}/{} ({:.3f}%)\tLoss: {:.6f}'.format(accurate_labels_val, all_labels_val, accuracy, loss))
