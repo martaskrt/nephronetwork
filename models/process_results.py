6c35f1f0ec4355fb7684cb1ea95ba1ee20e61233
martaskrt <skretam@mcmaster.ca>
2019-04-16 13:35:55 -0400

    added metrics for confusion matrix

--- models/process_results.py
+++ models/process_results.py
@@ -3,7 +3,7 @@
 
 from sklearn import metrics
 
-def get_metrics(y_score, y_true):
+def get_metrics(y_score, y_true, y_pred):
 
     fpr, tpr, thresholds = metrics.roc_curve(y_score=y_score, y_true=y_true)
     fnr = 1 - tpr
@@ -11,10 +11,16 @@ def get_metrics(y_score, y_true):
     auc = metrics.auc(fpr, tpr)
     auprc = metrics.average_precision_score(y_score=y_score, y_true=y_true)
 
+    tn, fp, fn, tp = metrics.confusion_matrix(y_pred=y_pred, y_true=y_true).ravel()
+
     results = {'fpr': fpr,
+               'fp': fp,
                'tpr': tpr,
+               'tp': tp,
                'fnr': fnr,
+               'fn': fn,
                'tnr': tnr,
+               'tn': tn,
                'auc': auc,
                'auprc': auprc}
 
