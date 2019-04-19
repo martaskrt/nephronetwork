26f8821477afe03ac3b79178dd781608d3c03542
martaskrt <skretam@mcmaster.ca>
2019-04-18 20:38:35 -0400

    formatted information that is saved every 5 epochs

--- models/siamese_network/train_siamese_network_CV.py
+++ models/siamese_network/train_siamese_network_CV.py
@@ -180,8 +180,8 @@ def train(args, train_X, train_y, test_X, test_y, max_epochs):
                                               y_true=all_targets_train.cpu().detach().numpy(),
                                               y_pred=all_pred_label_train.cpu().detach().numpy())
 
-        print('TrainEpoch\t{}\tACC\t{:.0f}%\tLoss\t{:.6f}\tAUC\t{:.6f}\t'
-              'AUPRC\t{:.6f}\tTN\t{}\tFP\t{}\tFN\t{}\tTP\t{}'.format(epoch, 100.*accurate_labels_train/counter_train,
+        print('TrainEpoch\t{}\tACC\t{:.6f}\tLoss\t{:.6f}\tAUC\t{:.6f}\t'
+              'AUPRC\t{:.6f}\tTN\t{}\tFP\t{}\tFN\t{}\tTP\t{}'.format(epoch, accurate_labels_train/counter_train,
                                                                     loss_accum_train/counter_train, results_train['auc'],
                                                                     results_train['auprc'], results_train['tn'],
                                                                     results_train['fp'], results_train['fn'],
@@ -197,23 +197,33 @@ def train(args, train_X, train_y, test_X, test_y, max_epochs):
         results_val = process_results.get_metrics(y_score=all_pred_prob_val.cpu().detach().numpy(),
                                                   y_true=all_targets_val.cpu().detach().numpy(),
                                                   y_pred=all_pred_label_val.cpu().detach().numpy())
-        print('ValEpoch\t{}\tACC\t{:.0f}%\tLoss\t{:.6f}\tAUC\t{:.6f}\t'
-              'AUPRC\t{:.6f}\tTN\t{}\tFP\t{}\tFN\t{}\tTP\t{}'.format(epoch, 100. * accurate_labels_val / counter_val,
+        print('ValEpoch\t{}\tACC\t{:.6f}\tLoss\t{:.6f}\tAUC\t{:.6f}\t'
+              'AUPRC\t{:.6f}\tTN\t{}\tFP\t{}\tFN\t{}\tTP\t{}'.format(epoch, accurate_labels_val / counter_val,
                                                                      loss_accum_val / counter_val, results_val['auc'],
                                                                      results_val['auprc'], results_val['tn'],
                                                                      results_val['fp'], results_val['fn'],
                                                                      results_val['tp']))
-            #
-            # if ((epoch+1) % 10) == 0:
-            #     checkpoint = {'epoch': epoch,
-            #                   'loss': loss,
-            #                   'hyperparams': hyperparams,
-            #                   'model_state_dict': net.state_dict(),
-            #                   'optimizer': optimizer.state_dict()}
-            #     if not os.path.isdir(args.dir):
-            #         os.makedirs(args.dir)
-            #     path_to_checkpoint = args.dir + '/' + "checkpoint_" + str(epoch) + '.pth'
-            #     torch.save(checkpoint, path_to_checkpoint)
+
+        if ((epoch+1) % 5) == 0 and epoch > 0:
+            checkpoint = {'epoch': epoch,
+                          'loss': loss,
+                          'hyperparams': hyperparams,
+                          'model_state_dict': net.state_dict(),
+                          'optimizer': optimizer.state_dict(),
+                          'loss_train': loss_accum_train / counter_train,
+                          'loss_val': loss_accum_val / counter_val,
+                          'accuracy_train': accurate_labels_train / counter_train,
+                          'accuracy_val': accurate_labels_val / counter_val,
+                          'results_train': results_train,
+                          'results_val': results_val,
+                          'all_pred_prob_train': all_pred_prob_train,
+                          'all_pred_prob_val': all_pred_prob_val,
+                          'all_targets_train': all_targets_train,
+                          'all_targets_val': all_targets_val}
+            if not os.path.isdir(args.dir):
+                os.makedirs(args.dir)
+            path_to_checkpoint = args.dir + '/' + "checkpoint_" + str(epoch) + '.pth'
+            torch.save(checkpoint, path_to_checkpoint)
 
 
 
