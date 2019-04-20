6b601d3601d6f17abad0ab9b7c925776660774c2
larunerdman <larunerdman1@gmail.com>
2019-04-20 00:37:53 +0000

    deleted matplotlib from imports

--- models/siamese_network/train_triamese_network_CV.py
+++ models/siamese_network/train_triamese_network_CV.py
@@ -3,7 +3,6 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import StratifiedKFold
 import codecs
 import errno
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 from PIL import Image
