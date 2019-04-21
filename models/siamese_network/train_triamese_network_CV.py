f389c559cd84dec400baf755b38ad59cbc742e9a
lauren.erdman@sickkids.ca <lauren.erdman@sickkids.ca>
2019-04-20 20:26:35 -0400

    Sorted dataloader to resolve memory issues

--- models/siamese_network/train_triamese_network_CV.py
+++ models/siamese_network/train_triamese_network_CV.py
@@ -63,6 +63,12 @@ def get_training_test_data(opt,split_batches = True,test_share = 0.1):
     return ts_train, tl_train, ts_test, tl_test
 
 
+def load_sample(infile):
+    in_df = pd.read_pickle(infile)
+    images  = in_df.triad
+    label = in_df.label
+    return images,label
+
 ## Create torch dataset from np dataset defined in previous function 
 class KidneyDataset(torch.utils.data.Dataset):
 
@@ -77,7 +83,18 @@ class KidneyDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.X)
 
-def train(args, train_X, train_y, test_X, test_y, max_epochs):
+class TriadDataset(torch.utils.data.Dataset):
+    def __init__(self,pickle_dir):
+        self.filelist = get_pickle_files(pickle_dir)
+
+    def __len__(self):
+        return len(self.filelist)
+    
+    def __getitem__(self,index):
+        imgs,target = load_sample(self.filelist[index])
+        return (imgs,target)
+
+def train(args, training_generator, validation_generator, max_epochs):
     net = SiamNet().to(device)
     if args.checkpoint != "":
         pretrained_dict = torch.load(args.checkpoint)
@@ -101,14 +118,10 @@ def train(args, train_X, train_y, test_X, test_y, max_epochs):
                    'momentum': args.momentum,
                    'weight_decay': args.weight_decay
                 }
-    # optimizer = torch.optim.SGD(net.parameters(), lr=hyperparams['lr'], momentum=hyperparams['momentum'],
-    #                             weight_decay=hyperparams['weight_decay'])
+    optimizer = torch.optim.SGD(net.parameters(), lr=hyperparams['lr'], momentum=hyperparams['momentum'],
+                                weight_decay=hyperparams['weight_decay'])
 
-    optimizer = torch.optim.Adam(net.parameters(), lr=hyperparams['lr'], weight_decay=hyperparams['weight_decay'])
-
-    params = {'batch_size': args.batch_size,
-              'shuffle': True,
-              'num_workers': args.num_workers}
+    # optimizer = torch.optim.Adam(net.parameters(), lr=hyperparams['lr'], weight_decay=hyperparams['weight_decay'])
 
 
     for epoch in range(max_epochs):
@@ -126,101 +139,86 @@ def train(args, train_X, train_y, test_X, test_y, max_epochs):
         all_pred_prob_val = []
         all_pred_label_val = []
 
-        n_splits = 5
         counter_train = 0
         counter_val = 0
-        skf = StratifiedKFold(n_splits=n_splits)
-        train_y = np.array(train_y)
-        for train_index, test_index in skf.split(train_X, train_y):
-            train_X_CV = train_X[train_index]
-            train_y_CV = train_y[train_index]
-            val_X_CV = train_X[test_index]
-            val_y_CV = train_y[test_index]
+# =============================================================================
+#         for train_index, test_index in skf.split(train_X, train_y):
+#             train_X_CV = train_X[train_index]
+#             train_y_CV = train_y[train_index]
+#             val_X_CV = train_X[test_index]
+#             val_y_CV = train_y[test_index]
+# 
+#             training_set = KidneyDataset(train_X_CV, train_y_CV)
+#             training_generator = DataLoader(training_set, **params)
+# 
+#             validation_set = KidneyDataset(val_X_CV, val_y_CV)
+#             validation_generator = DataLoader(validation_set, **params)
+# 
+# =============================================================================
+        for batch_idx, (data, target) in enumerate(training_generator):
+            data = torch.stack(data)
+            data = torch.transpose(data,0,1).float()  
+            
+            optimizer.zero_grad()
+            output = net(data)
+            target = Variable(target.type(torch.LongTensor), requires_grad=False).to(device)
 
-            training_set = KidneyDataset(train_X_CV, train_y_CV)
-            training_generator = DataLoader(training_set, **params)
+            loss = F.cross_entropy(output, target)
+            loss_accum_train += loss.item() * len(target)
 
-            validation_set = KidneyDataset(val_X_CV, val_y_CV)
-            validation_generator = DataLoader(validation_set, **params)
+            loss.backward()
 
-            for batch_idx, (data, target) in enumerate(training_generator):
+            accurate_labels_train += torch.sum(torch.argmax(output, dim=1) == target).cpu()
+            optimizer.step()
+            counter_train += len(target)
+
+            output_softmax = softmax(output)
+            pred_prob = output_softmax[:, 1]
+            pred_label = torch.argmax(output_softmax, dim=1)
+
+            assert len(pred_prob) == len(target)
+            assert len(pred_label) == len(target)
+
+            all_pred_prob_train.append(pred_prob)
+            all_targets_train.append(target)
+            all_pred_label_train.append(pred_label)
+
+        with torch.set_grad_enabled(False):
+            for batch_idx, (data, target) in enumerate(validation_generator):
+                data = torch.stack(data)
+                data = torch.transpose(data,0,1).float()  
+                net.zero_grad()
                 optimizer.zero_grad()
-
-                output = net(data.to(device))
-                target = Variable(target.type(torch.LongTensor), requires_grad=False).to(device)
+                output = net(data)
+                target = target.type(torch.LongTensor).to(device)
 
                 loss = F.cross_entropy(output, target)
-                loss_accum_train += loss.item() * len(target)
-
-                loss.backward()
-
-                accurate_labels_train += torch.sum(torch.argmax(output, dim=1) == target).cpu()
-                optimizer.step()
-                counter_train += len(target)
-
+                loss_accum_val += loss.item() * len(target)
+                counter_val += len(target)
                 output_softmax = softmax(output)
-                pred_prob = output_softmax[:, 1]
-                pred_label = torch.argmax(output_softmax, dim=1)
+
+                accurate_labels_val += torch.sum(torch.argmax(output, dim=1) == target).cpu()
+
+                pred_prob = output_softmax[:,1]
+                pred_prob = pred_prob.squeeze()
+                pred_label = torch.argmax(output, dim=1)
 
                 assert len(pred_prob) == len(target)
                 assert len(pred_label) == len(target)
 
-                all_pred_prob_train.append(pred_prob)
-                all_targets_train.append(target)
-                all_pred_label_train.append(pred_label)
-
-            with torch.set_grad_enabled(False):
-                for batch_idx, (data, target) in enumerate(validation_generator):
-                    net.zero_grad()
-                    optimizer.zero_grad()
-                    output = net(data)
-                    target = target.type(torch.LongTensor).to(device)
-
-                    loss = F.cross_entropy(output, target)
-                    loss_accum_val += loss.item() * len(target)
-                    counter_val += len(target)
-                    output_softmax = softmax(output)
-
-                    accurate_labels_val += torch.sum(torch.argmax(output, dim=1) == target).cpu()
-
-                    pred_prob = output_softmax[:,1]
-                    pred_prob = pred_prob.squeeze()
-                    pred_label = torch.argmax(output, dim=1)
-
-                    assert len(pred_prob) == len(target)
-                    assert len(pred_label) == len(target)
-
-                    all_pred_prob_val.append(pred_prob)
-                    all_targets_val.append(target)
-                    all_pred_label_val.append(pred_label)
+                all_pred_prob_val.append(pred_prob)
+                all_targets_val.append(target)
+                all_pred_label_val.append(pred_label)
 
 
-        all_pred_prob_train = torch.cat(all_pred_prob_train)
-        all_targets_train = torch.cat(all_targets_train)
-        all_pred_label_train = torch.cat(all_pred_label_train)
-
-        assert len(all_targets_train) == len(train_y)*(n_splits-1)
-        assert len(all_pred_prob_train) == len(train_y)*(n_splits-1)
-        assert len(all_pred_label_train) == len(train_y)*(n_splits-1)
-
-        results_train = process_results.get_metrics(y_score=all_pred_prob_train.cpu().detach().numpy(),
-                                              y_true=all_targets_train.cpu().detach().numpy(),
-                                              y_pred=all_pred_label_train.cpu().detach().numpy())
-
-        print('TrainEpoch\t{}\tACC\t{:.0f}%\tLoss\t{:.6f}\tAUC\t{:.6f}\t'
-              'AUPRC\t{:.6f}\tTN\t{}\tFP\t{}\tFN\t{}\tTP\t{}'.format(epoch, 100.*accurate_labels_train/counter_train,
-                                                                    loss_accum_train/counter_train, results_train['auc'],
-                                                                    results_train['auprc'], results_train['tn'],
-                                                                    results_train['fp'], results_train['fn'],
-                                                                    results_train['tp']))
         all_pred_prob_val = torch.cat(all_pred_prob_val)
         all_targets_val = torch.cat(all_targets_val)
         all_pred_label_val = torch.cat(all_pred_label_val)
-
-        assert len(all_targets_val) == len(train_y)
-        assert len(all_pred_prob_val) == len(train_y)
-        assert len(all_pred_label_val) == len(train_y)
-
+    
+        #assert len(all_targets_val) == len(train_y)
+        #assert len(all_pred_prob_val) == len(train_y)
+        #assert len(all_pred_label_val) == len(train_y)
+    
         results_val = process_results.get_metrics(y_score=all_pred_prob_val.cpu().detach().numpy(),
                                                   y_true=all_targets_val.cpu().detach().numpy(),
                                                   y_pred=all_pred_label_val.cpu().detach().numpy())
@@ -246,7 +244,8 @@ def train(args, train_X, train_y, test_X, test_y, max_epochs):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_dir', default='/home/lauren/train-us-seqs',help="Number of epochs")
+    parser.add_argument('--train_dir', default='/home/lauren/ind_train_us_seqs',help="Number of epochs")
+    parser.add_argument('--valid_dir', default='/home/lauren/ind_val_us_seqs', type=int, help="Number of epochs")
     parser.add_argument('--epochs', default=50, type=int, help="Number of epochs")
     parser.add_argument('--batch_size', default=256, type=int, help="Batch size")
     parser.add_argument('--lr', default=0.001, type=float, help="Learning rate")
@@ -258,14 +257,31 @@ def main():
     parser.add_argument("--checkpoint", default="", help="Path to load pretrained model checkpoint from")
     parser.add_argument("--git_dir", default="/home/lauren/nephronetwork", help="Path to github repo with necessary modules to load")
     parser.add_argument("--siam_unet", default=False, help="Use Regular Siam Net or Siame UNet")
-
+    
     args = parser.parse_args()
 
-    max_epochs = args.epochs
-
-    train_X,train_y,test_X,test_y = get_training_test_data(args)
+    process_results = importlib.machinery.SourceFileLoader('process_results',os.path.join(args.git_dir,'models/process_results.py')).load_module()
     
-    train(args,train_X,train_y,test_X,test_y,max_epochs)
+    os.chdir(args.git_dir +'/models/siamese_network/')
+    if args.siam_unet:
+        from SiameseNetworkUNet import SiamNet
+    else:
+        from SiameseNetwork import SiamNet
+    
+    max_epochs = args.epochs
+    
+    training_triad_data = TriadDataset(args.train_dir)
+    
+    triad_training_dataloader = torch.utils.data.DataLoader(dataset=training_triad_data,
+                                                   batch_size=args.batch_size,
+                                                   shuffle=True)
+    
+    valid_triad_data = TriadDataset(args.valid_dir)
+    triad_valid_dataloader = torch.utils.data.DataLoader(dataset=valid_triad_data,
+                                                   batch_size=args.batch_size,
+                                                   shuffle=True)
+ 
+    train(args,triad_training_dataloader, triad_valid_dataloader,max_epochs)
 
 if __name__ == '__main__':
     main()
@@ -280,7 +296,8 @@ if __name__ == '__main__':
 #     pass
 # args = options()
 # 
-# args.train_dir = 'C:/Users/larun/Desktop/Data Science Core/Projects/Urology/Image-analysis/triad/training-dir'
+# args.train_dir = 'C:/Users/larun/Desktop/Data Science Core/Projects/Urology/Image-analysis/test-dir/test/'
+# args.valid_dir = 'C:/Users/larun/Desktop/Data Science Core/Projects/Urology/Image-analysis/test-dir/test/'
 # args.epochs = 4
 # args.batch_size = 32
 # args.lr = 0.001
@@ -303,9 +320,42 @@ if __name__ == '__main__':
 # 
 # max_epochs = args.epochs
 # 
-# train_X,train_y,test_X,test_y = get_training_test_data(args)
+# training_triad_data = TriadDataset(args.train_dir)
+# 
+# triad_training_dataloader = torch.utils.data.DataLoader(dataset=training_triad_data,
+#                                                batch_size=args.batch_size,
+#                                                shuffle=True)
+# 
+# valid_triad_data = TriadDataset(args.valid_dir)
+# triad_valid_dataloader = torch.utils.data.DataLoader(dataset=valid_triad_data,
+#                                                batch_size=args.batch_size,
+#                                                shuffle=True)
+# 
+# 
+# 
+# for batch_idx, (data,labels) in enumerate(triad_training_dataloader):
+#     
+#     data = torch.stack(data)
+#     data = torch.transpose(data,0,1).float()  
+#     print(data.type())
+# 
+# 
+# ## batchdata = output[1][0]
+#     
+# 
+# 
+# training_triad_data.__len__()
+# 
+# for (imgs,target) in enumerate(training_triad_data):
+#     print((imgs,target))
+# 
+# 
+# batch = random.sample(range(training_triad_data.__len__()),32)
+# 
+# my_batch = triad_data.__getitem__(batch)
 # 
 # train(args,train_X,train_y,test_X,test_y,max_epochs)
 # 
+# 
+# 
 # =============================================================================
-
