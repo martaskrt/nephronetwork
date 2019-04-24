0af0177fdf184c869c5881899b4fc337063477dd
martaskrt <skretam@mcmaster.ca>
2019-04-24 11:17:43 -0400

    debugging mismatching dimensions

--- models/siamese_network/SiameseNetworkUNet.py
+++ models/siamese_network/SiameseNetworkUNet.py
@@ -39,8 +39,8 @@ class SiamNet(nn.Module):
         # *************************** changed layers *********************** #
 
         self.fc6 = nn.Sequential()
-        self.fc6.add_module('fc6_s1', nn.Conv2d(256, 1024, kernel_size=3, stride=1, padding=1))
-        # self.fc6.add_module('fc6_s1', nn.Conv2d(256, 1024, kernel_size=2, stride=1, padding=1))
+        # self.fc6.add_module('fc6_s1', nn.Conv2d(256, 1024, kernel_size=3, stride=1, padding=1))
+        self.fc6.add_module('fc6_s1', nn.Conv2d(256, 1024, kernel_size=2, stride=1, padding=1))
         self.fc6.add_module('batch6_s1', nn.BatchNorm2d(1024))
 
         # self.fc6b = nn.Sequential()
