bb053b156d8faeeefeb353904e69034feb44bc1b
martaskrt <skretam@mcmaster.ca>
2019-05-15 16:14:17 -0400

    fixed accesing best val epoch

--- postprocess/process_model_output_file.py
+++ postprocess/process_model_output_file.py
@@ -40,27 +40,32 @@ def load_data(inputfile):
 
 
 def get_metric_results(data, metric, early_stop_epoch):
-    max_avg_epochs_results = {}
-    max_avg_epochs_results['train'] = 0
-    max_avg_epochs_results['val'] = 0
-    max_avg_epochs_results['test'] = 0
 
     avg_of_val_epochs = np.array(data[1]['val'][metric])
     for fold in range(2, 6):
         avg_of_val_epochs = np.sum((avg_of_val_epochs, np.array(data[fold]['val'][metric])), axis=0)
     avg_of_val_epochs /= 5
-    avg_of_val_epochs = np.array(avg_of_val_epochs)[:early_stop_epoch + 1]
+    avg_of_val_epochs = np.array(avg_of_val_epochs)[:early_stop_epoch+1]
 
     max_of_avg_epochs = int(np.argmax(avg_of_val_epochs))
 
-    for fold in range(1, 6):
-        max_avg_epochs_results['train'] += data[fold]['train'][metric][max_of_avg_epochs]
-        max_avg_epochs_results['val'] += data[fold]['val'][metric][max_of_avg_epochs]
-        max_avg_epochs_results['test'] += data[fold]['test'][metric][max_of_avg_epochs]
+    print("best_val_epoch\t{}".format(max_of_avg_epochs))
+    max_avg_epochs_results = {}
+    max_avg_epochs_results['train'] = {}
+    max_avg_epochs_results['val'] = {}
+    max_avg_epochs_results['test'] = {}
+    for metric in ["AUC", "AUPRC"]:
+        max_avg_epochs_results['train'][metric] = 0
+        max_avg_epochs_results['val'][metric] = 0
+        max_avg_epochs_results['test'][metric] = 0
+        for fold in range(1, 6):
+            max_avg_epochs_results['train'][metric] += data[fold]['train'][metric][max_of_avg_epochs]
+            max_avg_epochs_results['val'][metric] += data[fold]['val'][metric][max_of_avg_epochs]
+            max_avg_epochs_results['test'][metric] += data[fold]['test'][metric][max_of_avg_epochs]
 
-    max_avg_epochs_results['train'] /= 5
-    max_avg_epochs_results['val'] /= 5
-    max_avg_epochs_results['test'] /= 5
+        max_avg_epochs_results['train'][metric] /= 5
+        max_avg_epochs_results['val'][metric] /= 5
+        max_avg_epochs_results['test'][metric] /= 5
 
     return max_avg_epochs_results
 
@@ -68,9 +73,10 @@ def get_metric_results(data, metric, early_stop_epoch):
 def compute_results(filename, data, early_stop_epoch):
     print("FILE NAME......................................" + str(filename))
     print("early_stop_epoch\t{}".format(early_stop_epoch))
-    auc = get_metric_results(data, 'AUC', early_stop_epoch)
-    auprc = get_metric_results(data, 'AUPRC', early_stop_epoch)
-    print('{:.3f}/{:.3f}\t{:.3f}/{:.3f}\t{:.3f}/{:.3f}'.format(auc['train'], auprc['train'], auc['val'], auprc['val'],
-                                                               auc['test'], auprc['test']))
+    metrics = get_metric_results(data, 'AUC', early_stop_epoch)
+    # auprc = get_metric_results(data, 'AUPRC', early_stop_epoch)
+    print('{:.3f}/{:.3f}\t{:.3f}/{:.3f}\t{:.3f}/{:.3f}'.format(metrics['train']["AUC"], metrics['train']["AUPRC"],
+                                                               metrics['val']["AUC"], metrics['val']["AUPRC"],
+                                                               metrics['test']["AUC"], metrics['test']["AUPRC"]))
     print("**********************************************************************************************")
 
