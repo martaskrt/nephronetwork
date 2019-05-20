a682c4c672546bdea20e1b9c692a19a686e9af5b
Marta <marta.skreta@sickkids.ca>
2019-05-20 20:00:12 +0000

    updated training files

--- models/siamese_network/runabunchofmodels.sh
+++ models/siamese_network/runabunchofmodels.sh
@@ -1,6 +1,6 @@
 var1="mountpoint/"
 var2=".txt"
-dir_name="unet_20190517_vanilla_CV_crop0_OR_plusRef_BN_cwpen2"
-python3 train_siamese_network_CV_v2.py --dir "$dir_name" --epochs 35  --view 'siamese' --crop 0 --unet > "$dir_name$var2" && mv "$dir_name$var2" "$dir_name"
+dir_name="siamnet_20190520_vanilla_siamese"
+python3 train_siamese_network_CV_v2.py --dir "$var1$dir_name" --epochs 35  --view 'siamese' > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
 
 
