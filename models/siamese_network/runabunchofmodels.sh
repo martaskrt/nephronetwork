7dcd1270c48c5d360b4407d1c0caf891dab292e1
Marta <marta.skreta@sickkids.ca>
2019-05-20 19:18:53 +0000

    script to train models

---
+++ models/siamese_network/runabunchofmodels.sh
@@ -0,0 +1,6 @@
+var1="mountpoint/"
+var2=".txt"
+dir_name="unet_20190517_vanilla_CV_crop0_OR_plusRef_BN_cwpen2"
+python3 train_siamese_network_CV_v2.py --dir "$var1$dir_name" --epochs 35  --view 'siamese' --crop 0 --unet > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
+
+
