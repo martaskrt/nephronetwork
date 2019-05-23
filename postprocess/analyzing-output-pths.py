a9a37f9b07579e0f839e824ddcbfdc7f513cd831
lauren.erdman@sickkids.ca <lauren.erdman@sickkids.ca>
2019-05-23 11:18:28 -0400

    made script work for single fold

--- postprocess/analyzing-output-pths.py
+++ postprocess/analyzing-output-pths.py
@@ -43,12 +43,12 @@ def get_fold(file_name):
     return fold
 
 ## CREAT PANDAS DATAFRAME FOR EACH FOLD
-def make_subset_df(my_fold_data,fold):
+def make_subset_wfold_df(my_fold_data,fold):
     
     # 0: study_id, 1: age_at_baseline, 2: gender (0 if male), 3: view (0 if saggital)...skip), 
     # 4: sample_num, 5: kidney side, 6: date_of_US_1, 7: date of curr US
     # print("ID splits: " +  ' '.join(my_fold_data['patient_ID_train'][1].split("_")))
-
+    
     print("Number of train samples: " + str(len(my_fold_data['patient_ID_train'])))
     train_dict = {'full_ID': my_fold_data['patient_ID_train'],
                 'Fold': [fold]*len(my_fold_data['patient_ID_train']),
@@ -97,37 +97,97 @@ def make_subset_df(my_fold_data,fold):
 
     return train_df,val_df,test_df
 
-def make_full_df(pth_folder):
+## CREAT PANDAS DATAFRAME FOR EACH FOLD
+def make_subset_nofold_df(my_fold_data):
+    
+    # 0: study_id, 1: age_at_baseline, 2: gender (0 if male), 3: view (0 if saggital)...skip), 
+    # 4: sample_num, 5: kidney side, 6: date_of_US_1, 7: date of curr US
+    # print("ID splits: " +  ' '.join(my_fold_data['patient_ID_train'][1].split("_")))
+    
+    print("Number of train samples: " + str(len(my_fold_data['patient_ID_train'])))
+    train_dict = {'full_ID': my_fold_data['patient_ID_train'],
+                'Target': my_fold_data['all_targets_train'].tolist(),
+                'Pred_val': my_fold_data['all_pred_prob_train'].tolist(),
+                'study_id': [x.split("_")[0] for x in my_fold_data['patient_ID_train']],
+                'age_at_baseline': [x.split("_")[1] for x in my_fold_data['patient_ID_train']],
+                'gender': [x.split("_")[2] for x in my_fold_data['patient_ID_train']],
+                'us_num': [x.split("_")[3] for x in my_fold_data['patient_ID_train']],
+                'kidney_side': [x.split("_")[4] for x in my_fold_data['patient_ID_train']],
+                'date_of_us1': [x.split("_")[5] for x in my_fold_data['patient_ID_train']],
+                'date_of_current_us': [x.split("_")[6] for x in my_fold_data['patient_ID_train']]}#,
+                #'us_man': [x.split("_")[7] for x in my_fold_data['patient_ID_train']]}
+    
+    print("Number of val samples: " + str(len(my_fold_data['patient_ID_val'])))
+    val_dict = {'full_ID': my_fold_data['patient_ID_val'],
+                'Target': my_fold_data['all_targets_val'].tolist(),
+                'Pred_val': my_fold_data['all_pred_prob_val'].tolist(),
+                'study_id': [x.split("_")[0] for x in my_fold_data['patient_ID_val']],
+                'age_at_baseline': [x.split("_")[1] for x in my_fold_data['patient_ID_val']],
+                'gender': [x.split("_")[2] for x in my_fold_data['patient_ID_val']],
+                'us_num': [x.split("_")[3] for x in my_fold_data['patient_ID_val']],
+                'kidney_side': [x.split("_")[4] for x in my_fold_data['patient_ID_val']],
+                'date_of_us1': [x.split("_")[5] for x in my_fold_data['patient_ID_val']],
+                'date_of_current_us': [x.split("_")[6] for x in my_fold_data['patient_ID_val']]}#,
+                #'us_man': [x.split("_")[7] for x in my_fold_data['patient_ID_val']]}
+
+    print("Number of test samples: " + str(len(my_fold_data['patient_ID_test'])))
+    test_dict = {'full_ID': my_fold_data['patient_ID_test'],
+                'Target': my_fold_data['all_targets_test'].tolist(),
+                'Pred_val': my_fold_data['all_pred_prob_test'].tolist(),
+                'study_id': [x.split("_")[0] for x in my_fold_data['patient_ID_test']],
+                'age_at_baseline': [x.split("_")[1] for x in my_fold_data['patient_ID_test']],
+                'gender': [x.split("_")[2] for x in my_fold_data['patient_ID_test']],
+                'us_num': [x.split("_")[3] for x in my_fold_data['patient_ID_test']],
+                'kidney_side': [x.split("_")[4] for x in my_fold_data['patient_ID_test']],
+                'date_of_us1': [x.split("_")[5] for x in my_fold_data['patient_ID_test']],
+                'date_of_current_us': [x.split("_")[6] for x in my_fold_data['patient_ID_test']]}#,
+                #'us_man': [x.split("_")[7] for x in my_fold_data['patient_ID_test']]}
+
+    train_df = pd.DataFrame.from_dict(train_dict)
+    val_df = pd.DataFrame.from_dict(val_dict)
+    test_df = pd.DataFrame.from_dict(test_dict)
+
+    return train_df,val_df,test_df
+
+    
+def make_full_df(pth_folder,cv):
     my_pth_files = load_pth_files_names(pth_folder)
     
-    for file_name in my_pth_files: 
-    
-        fold_data = open_file(file_name)
-        fold_num = get_fold(file_name)
-        if file_name == my_pth_files[0]:
-            my_train_df, my_val_df, my_test_df = make_subset_df(fold_data,fold_num)
-            print(file_name + " pandas dataframes created.")
-        else:
-            new_train,new_val,new_test = make_subset_df(fold_data,fold_num)
-            print(file_name + " pandas dataframes created.")
-            #print(new_train.shape)
-            my_train_df = my_train_df.append(new_train)
-            my_val_df = my_val_df.append(new_val)
-            my_test_df = my_test_df.append(new_test)
-
+    if cv:     
+        for file_name in my_pth_files: 
+        
+            fold_data = open_file(file_name)
+            fold_num = get_fold(file_name)
+            if file_name == my_pth_files[0]:
+                my_train_df, my_val_df, my_test_df = make_subset_wfold_df(fold_data,fold_num)
+                print(file_name + " pandas dataframes created.")
+            else:
+                new_train,new_val,new_test = make_subset_wfold_df(fold_data,fold_num)
+                print(file_name + " pandas dataframes created.")
+                #print(new_train.shape)
+                my_train_df = my_train_df.append(new_train)
+                my_val_df = my_val_df.append(new_val)
+                my_test_df = my_test_df.append(new_test)
+    else: 
+        assert len(my_pth_files) == 1
+        mydata = open_file(my_pth_files)
+        my_train_df, my_val_df, my_test_df = make_subset_nofold_df(fold_data)
+        print(file_name + " pandas dataframes created.")
+                
     print("Final training dataframe shape: \n")
-    print(my_train_df.shape)            
+    # print(my_train_df.shape)            
     return my_train_df, my_val_df, my_test_df
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--checkpoint_folder', help="Number of epochs")
+    parser.add_argument('--cv', help="Number of epochs")
 
     args = parser.parse_args()
 
-    train,val,test = make_full_df(args.checkpoint_folder)
-    
+    train,val,test = make_full_df(args.checkpoint_folder,args.cv)
+
     file_root = args.checkpoint_folder.split("/")[len(args.checkpoint_folder.split("/")) - 1]
     
     train.to_csv(args.checkpoint_folder+ "/" + file_root + "_train.csv")
