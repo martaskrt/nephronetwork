708041b23828749a1843e008873d0c9ccce0b11b
lauren.erdman@sickkids.ca <lauren.erdman@sickkids.ca>
2019-05-23 11:31:53 -0400

    fixed validation bug

--- postprocess/analyzing-output-pths.py
+++ postprocess/analyzing-output-pths.py
@@ -116,20 +116,7 @@ def make_subset_nofold_df(my_fold_data):
                 'date_of_us1': [x.split("_")[5] for x in my_fold_data['patient_ID_train']],
                 'date_of_current_us': [x.split("_")[6] for x in my_fold_data['patient_ID_train']]}#,
                 #'us_man': [x.split("_")[7] for x in my_fold_data['patient_ID_train']]}
-    
-    print("Number of val samples: " + str(len(my_fold_data['patient_ID_val'])))
-    val_dict = {'full_ID': my_fold_data['patient_ID_val'],
-                'Target': my_fold_data['all_targets_val'].tolist(),
-                'Pred_val': my_fold_data['all_pred_prob_val'].tolist(),
-                'study_id': [x.split("_")[0] for x in my_fold_data['patient_ID_val']],
-                'age_at_baseline': [x.split("_")[1] for x in my_fold_data['patient_ID_val']],
-                'gender': [x.split("_")[2] for x in my_fold_data['patient_ID_val']],
-                'us_num': [x.split("_")[3] for x in my_fold_data['patient_ID_val']],
-                'kidney_side': [x.split("_")[4] for x in my_fold_data['patient_ID_val']],
-                'date_of_us1': [x.split("_")[5] for x in my_fold_data['patient_ID_val']],
-                'date_of_current_us': [x.split("_")[6] for x in my_fold_data['patient_ID_val']]}#,
-                #'us_man': [x.split("_")[7] for x in my_fold_data['patient_ID_val']]}
-
+ 
     print("Number of test samples: " + str(len(my_fold_data['patient_ID_test'])))
     test_dict = {'full_ID': my_fold_data['patient_ID_test'],
                 'Target': my_fold_data['all_targets_test'].tolist(),
