bba80f5d37be98f9103338dc16ad98bdd99c3c74
Marta <marta.skreta@sickkids.ca>
2019-06-12 15:58:58 +0000

    updated files rip

--- 1.Models/siamese_network/runabunchofmodels.sh
+++ 1.Models/siamese_network/runabunchofmodels.sh
@@ -6,11 +6,38 @@ var2=".txt"
 #mkdir mountpoint/20190601_CVModels
 #mkdir mountpoint/20190601_fullModels
 
-var1="mountpoint/20190601_CVModels/"
-var3="mountpoint/20190601_fullModels/"
+var1="mountpoint/20190611_CVModels/"
+var3="mountpoint/20190611_fullModels/"
+
+#mkdir $var1
+#mkdir $var3
 
 
 
+dir_name="unet_20190611_vanilla_siamese_upconv_1c_1ch_contrast2_1dropout0.2"
+python3 train_siamese_network_CV_v2.py --unet --sc 0 --upconv 1 --contrast 2 --batch_size 128 --epochs 35 --view 'siamese' --output_dim 256 --dir "$var1$dir_name" > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
+
+dir_name="unet_20190611_vanilla_siamese_upconv_1c_1ch_contrast2_1dropout0.2_full"
+python3 train_siamese_network.py --unet --sc 0 --upconv 1 --contrast 2 --output_dim 256 --batch_size 128  --dir "$var3$dir_name" --epochs 35  --view 'siamese'  > "$dir_name$var2" && mv "$dir_name$var2" "$var3$dir_name"
+
+
+#dir_name="unet_20190606_vanilla_siamese_sc2"
+#python3 train_siamese_network_CV_v2.py --unet --sc 2 --batch_size 128 --epochs 35 --view 'siamese' --output_dim 256 --dir "$var1$dir_name" > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
+
+#dir_name="unet_20190606_vanilla_siamese_sc2_full"
+#python3 train_siamese_network.py --unet --sc 2 --output_dim 256 --batch_size 128  --dir "$var3$dir_name" --epochs 35  --view 'siamese'  > "$dir_name$var2" && mv "$dir_name$var2" "$var3$dir_name"
+
+#dir_name="unet_20190606_vanilla_siamese_sc1"
+#python3 train_siamese_network_CV_v2.py --unet --sc 1 --batch_size 128 --epochs 35 --view 'siamese' --output_dim 256 --dir "$var1$dir_name" > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
+
+#dir_name="unet_20190606_vanilla_siamese_sc1_full"
+#python3 train_siamese_network.py --unet --sc 1 --output_dim 256 --batch_size 128  --dir "$var3$dir_name" --epochs 35  --view 'siamese'  > "$dir_name$var2" && mv "$dir_name$var2" "$var3$dir_name"
+
+#dir_name="unet_20190606_vanilla_siamese_upconv"
+#python3 train_siamese_network_CV_v2.py --unet --sc 0 --batch_size 128 --epochs 35 --view 'siamese' --output_dim 256 --dir "$var1$dir_name" > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
+
+#dir_name="unet_20190606_vanilla_siamese_upconv_full"
+#python3 train_siamese_network.py --unet --sc 0 --output_dim 256 --batch_size 128  --dir "$var3$dir_name" --epochs 35  --view 'siamese'  > "$dir_name$var2" && mv "$dir_name$var2" "$var3$dir_name"
 #dir_name="siamnet_20190601_vanilla_siamese_gap"
 #python3 train_siamese_network_CV_v2.py --dir "$var1$dir_name" --epochs 35  --view 'siamese'  --output_dim 256 > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
 
@@ -23,11 +50,11 @@ var3="mountpoint/20190601_fullModels/"
 #dir_name="siamnet_20190601_vanilla_siamese_full"
 #python3 train_siamese_network.py --output_dim 256 --dir "$var3$dir_name" --epochs 35  --view 'siamese' > "$dir_name$var2" && mv "$dir_name$var2" "$var3$dir_name"
 
-dir_name="unet_20190601_vanilla_sag_2"
-python3 train_siamese_network_CV_v2.py --unet --dir "$var1$dir_name" --epochs 35  --view 'sag'  --output_dim 128 > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
+#dir_name="unet_20190601_vanilla_sag_2"
+#python3 train_siamese_network_CV_v2.py --unet --dir "$var1$dir_name" --epochs 35  --view 'sag'  --output_dim 128 > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
 
-dir_name="unet_20190601_vanilla_sag_full_2"
-python3 train_siamese_network.py --unet --output_dim 128 --dir "$var3$dir_name" --epochs 35  --view 'sag'  > "$dir_name$var2" && mv "$dir_name$var2" "$var3$dir_name"
+#dir_name="unet_20190601_vanilla_sag_full_2"
+#python3 train_siamese_network.py --unet --output_dim 128 --dir "$var3$dir_name" --epochs 35  --view 'sag'  > "$dir_name$var2" && mv "$dir_name$var2" "$var3$dir_name"
 
 #dir_name="siamnet_20190601_vanilla_trans"
 #python3 train_siamese_network_CV_v2.py --dir "$var1$dir_name" --epochs 35  --view 'trans' --output_dim 128 > "$dir_name$var2" && mv "$dir_name$var2" "$var1$dir_name"
