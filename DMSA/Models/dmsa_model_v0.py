44670cbc3e2586b51ab4b3f3b7853887245a5e36
larunerdman <larunerdman1@gmail.com>
2020-04-07 23:36:07 -0400

    finished data loader

--- DMSA/Models/dmsa_model_v0.py
+++ DMSA/Models/dmsa_model_v0.py
@@ -8,25 +8,48 @@ from torchvision import transforms
 import importlib.machinery
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
+import pandas as pd
 
 
-###
-###        DEFINING DATASETS
-###
+    ###
+    ###        HELPER FUNCTIONS
+    ###
+
+def read_image_file(file_name):
+
+    """
+    Reads in image file, crops it (if needed), converts it to greyscale, and returns image
+    :param file_name: path and name of file
+
+    :return: Torch tensor of image
+    """
+    return torch.from_numpy(np.asarray(Image.open(file_name).convert('L')))
+
+    ###
+    ###        DEFINING DATASETS
+    ###
+
+    ###
+    ###        DATASET
+    ###
 
 class DMSADataset(Dataset):
     """ Data loader for DMSA data """
 
-    def __init__(self,data_sheet,dichot = False):
+    def __init__(self, data_sheet, args, dim=256):
         """
         data_sheet: csv spreadsheet with 1 column for each US view image file ("SR","SL","TR","TL","B"),
             1 column for each DMSA view image file (A/P) ("DMSA_A" and "DMSA_P"),
             2 label columns: 1 dichotomous and 1 continuous ("FUNC_DICH" and "FUNC_CONT"), and
             1 column for the instance id "ID"
 
-        dichot: if set to True, a binary 0/1 value will be used as the label
+        args: argument dictionary
+            args.dichot: if set to True, a binary 0/1 value will be used as the label
             else if set to False, a continuous value for left function will be used as the label
         """
+        self.dim = dim
+
+        self.rootdir = args.rootdir
 
         self.id = data_sheet['ID']
 
@@ -39,7 +62,7 @@ class DMSADataset(Dataset):
         self.dmsa_a_file = data_sheet['DMSA_A']
         self.dmsa_p_file = data_sheet['DMSA_P']
 
-        if dichot:
+        if args.dichot:
             self.label = data_sheet['FUNC_DICH']
         else:
             self.label = data_sheet['FUNC_CONT']
@@ -49,21 +72,35 @@ class DMSADataset(Dataset):
 
     def __getitem__(self, index):
             ## Maybe add file path as argument to init? Maybe "opt"?
-        self.sr = Image.open(self.sr_file[index]).convert('L')
-        self.sl = Image.open(self.sl_file[index]).convert('L')
-        self.tr = Image.open(self.tr_file[index]).convert('L')
-        self.tl = Image.open(self.tl_file[index]).convert('L')
-        self.b = Image.open(self.b_file[index]).convert('L')
+        sr = read_image_file(self.rootdir + self.sr_file[index]).view(1, self.dim, self.dim)
+        sl = read_image_file(self.rootdir + self.sl_file[index]).view(1, self.dim, self.dim)
+        tr = read_image_file(self.rootdir + self.tr_file[index]).view(1, self.dim, self.dim)
+        tl = read_image_file(self.rootdir + self.tl_file[index]).view(1, self.dim, self.dim)
+        b = read_image_file(self.rootdir + self.b_file[index]).view(1, self.dim, self.dim)
             ## make 5x256x256 tensor
-        input_tensor = torch.tensor()
+        input_tensor = torch.cat((sr, sl, tr, tl, b), 0)
 
-        self.dmsa_a = Image.open(self.dmsa_a_file[index]).convert('L')
-        self.dmsa_p = Image.open(self.dmsa_p_file[index]).convert('L')
+        dmsa_a = read_image_file(self.rootdir + self.dmsa_a_file[index]).view(1, self.dim, self.dim)
+        dmsa_p = read_image_file(self.rootdir + self.dmsa_p_file[index]).view(1, self.dim, self.dim)
             ## make 2x256x256 tensor
-        output_tensor = torch.tensor()
+        output_tensor = torch.cat((dmsa_a, dmsa_p),0)
 
         out_label = torch.tensor(self.label[index])
 
-
         return input_tensor, output_tensor, out_label
+
+
+## to use dataset:
+
+## args for debugging :)
+class make_opt():
+    def __init__(self):
+        self.rootdir = 'C:/Users/larun/Desktop/Data Science Core/Projects/Urology/Image organization Nov 2019/'
+        # self.rootdir = '/hpf/largeprojects/agoldenb/lauren/Hydronephrosis/data/load_training_test_sets/'
+        self.dichot = False # for a 0/1 outcome, set to True
+
+opt = make_opt()
+my_datasheet=pd.read_csv(opt.rootdir + 'DMSA-datasheet-top3view.csv')
+my_data = DMSADataset(data_sheet=my_datasheet, args=opt)
+my_dataloader = DataLoader(my_data, shuffle=True)
 
