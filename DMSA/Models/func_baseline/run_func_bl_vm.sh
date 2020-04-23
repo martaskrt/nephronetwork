ebb5cfabf600d45e21fdb72bdc282d78161fec4d
Marta <marta.skreta@sickkids.ca>
2020-04-23 16:59:34 +0000

    updated baseline function run

--- DMSA/Models/func_baseline/run_func_bl_vm.sh
+++ DMSA/Models/func_baseline/run_func_bl_vm.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
 python3 /home/marta/nephronetwork-github/nephronetwork/DMSA/Models/func_baseline/func_bl_model.py \
-	-train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-train-datasheet-top2view-USfunc.csv \
-	-val_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-val-datasheet-top2view-USfunc.csv \
-	-test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-test-datasheet-top2view-USfunc.csv \
+	-train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-train-datasheet-top3view-USfunc-10ksamp.csv \
+	-val_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-val-datasheet-top3view-USfunc-10ksamp.csv \
+	-test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-test-datasheet-top3view-USfunc-10ksamp.csv \
 	-csv_outdir /home/marta/lauren \
 	-us_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/all-jpgs-dmsa/ \
 	-dmsa_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/all-dmsa-cabs/dmsa-jpgs/
