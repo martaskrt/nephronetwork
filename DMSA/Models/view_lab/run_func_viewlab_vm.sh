1e6a08aee366727e6ae4254b8cd8a59266d7016a
larunerdman <larunerdman1@gmail.com>
2020-04-23 22:25:18 -0400

    updated label+func model

--- DMSA/Models/view_lab/run_func_viewlab_vm.sh
+++ DMSA/Models/view_lab/run_func_viewlab_vm.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
 python3 /home/marta/nephronetwork-github/nephronetwork/DMSA/Models/view_lab/train_view_func.py \
-	-func_train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-train-datasheet-top3view-USfunc-noVlab.csv \
-	-func_val_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-val-datasheet-top3view-USfunc-noVlab.csv \
-	-func_test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-test-datasheet-top3view-USfunc-noVlab.csv \
-	-lab_train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/train-view_label_df_20200423.csv \
-	-lab_val_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/val-view_label_df_20200423.csv \
-	-lab_test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/test-view_label_df_20200423.csv \
+	-func_train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-train-datasheet-top3view-USfunc-noVlab-2056CUT.csv \
+	-func_val_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-val-datasheet-top3view-USfunc-noVlab-2056CUT.csv \
+	-func_test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-test-datasheet-top3view-USfunc-noVlab-2056CUT.csv \
+	-lab_train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/train-view_label_df_20200423-2056CUT.csv \
+	-lab_val_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/val-view_label_df_20200423-2056CUT.csv \
+	-lab_test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/test-view_label_df_20200423-2056CUT.csv \
 	-csv_outdir /home/marta/lauren \
 	-us_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/all-jpgs-dmsa/ \
 	-lab_us_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/label_img/ 
