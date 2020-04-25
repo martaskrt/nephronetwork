bca130d7ce0940b69c516fa2d02be2b9692c18ef
Marta <marta.skreta@sickkids.ca>
2020-04-25 21:49:27 +0000

    updated vm model run

--- DMSA/Models/view_lab/run_func_viewlab_vm.sh
+++ DMSA/Models/view_lab/run_func_viewlab_vm.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 python3 /home/marta/nephronetwork-github/nephronetwork/DMSA/Models/view_lab/train_view_func.py \
-	-func_train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-train-datasheet-top3view-USfunc-noVlab.csv \
+	-func_train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_fulltrain_test_sets/DMSA-train-datasheet-top3view-USfunc-noVlab.csv \
 	-func_val_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-val-datasheet-top3view-USfunc-noVlab.csv \
 	-func_test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-test-datasheet-top3view-USfunc-noVlab.csv \
 	-lab_train_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/train-view_label_df_20200423-noOther.csv \
@@ -11,4 +11,4 @@ python3 /home/marta/nephronetwork-github/nephronetwork/DMSA/Models/view_lab/trai
 	-us_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/all-jpgs-dmsa/ \
 	-lab_us_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/label_img/ \
 	-lr 0.001 \
-	-max_epochs 50	
+	-max_epochs 14	
