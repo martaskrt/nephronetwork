71f5b983052c31f9ac7a071211513b098395c19e
Marta <marta.skreta@sickkids.ca>
2020-04-26 21:53:47 +0000

    sorting bl func auc

--- DMSA/Models/func_baseline/run_func_bl_vm.sh
+++ DMSA/Models/func_baseline/run_func_bl_vm.sh
@@ -6,7 +6,7 @@ python3 /home/marta/nephronetwork-github/nephronetwork/DMSA/Models/func_baseline
 	-test_datasheet /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/data/load_training_test_sets/DMSA-test-datasheet-top3view-USfunc-10ksamp.csv \
 	-csv_outdir /home/marta/lauren \
 	-us_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/all-jpgs-dmsa/ \
-	-dmsa_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/all-dmsa-cabs/dmsa-jpgs/
+	-dmsa_dir /home/marta/nephronetwork-github/nephronetwork/1.Models/siamese_network/mountpoint/all-dmsa-cabs/dmsa-jpgs/ \
 	-dichot
 	
 	
