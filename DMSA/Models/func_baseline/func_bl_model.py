5f519cffb11431d4e93657f38cec489641d82b5d
larunerdman <larunerdman1@gmail.com>
2020-04-26 17:46:40 -0400

    grabbing AUC from model runs

--- DMSA/Models/func_baseline/func_bl_model.py
+++ DMSA/Models/func_baseline/func_bl_model.py
@@ -597,6 +597,9 @@ def training_loop(args, network, file_lab):
             else:
                 epoch_train_pred.append(out.to("cpu").tolist())
 
+
+        print(epoch_train_lab)
+        print(epoch_train_pred)
         train_auc = roc_auc_score(np.array(flatten_list(epoch_train_lab)), np.array(flatten_list(epoch_train_pred)))
         print("Train AUC : " + str(train_auc))
         func_train_epoch_auc.append(train_auc)
